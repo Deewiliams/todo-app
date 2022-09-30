6e60e6d48c615024907b19f32b78e4f5d210999a
Desire <desireirankundawilliams@gmail.com>
2022-09-30 08:29:22 +0200

    created an input

---
+++ todo.js
@@ -0,0 +1,7 @@
+
+
+
+const todoInput = document.getElementById("new-task");
+const addButton = document.getElementsByTagName("button")[0];
+console.log('input',todoInput);
+console.log('input',addButton);
