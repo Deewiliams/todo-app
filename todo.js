71ddd6bdf08814e6632395fccfb58cb48c33e511
Desire <desireirankundawilliams@gmail.com>
2022-09-30 08:56:11 +0200

    able to display a table list of tasks

--- todo.js
+++ todo.js
@@ -1,7 +1,31 @@
 
 
+document.querySelector("#add").onclick = function () {
+  if (document.querySelector("#newtask input").value.length == 0) {
+    alert("Please Enter a Task");
+  } else {
+    document.querySelector("#tasks").innerHTML += `
+            <div class="task"><table>
+            <tr>
+              <th>To do list</th>
+            </tr>
+            <tr id="taskname">
+              <td>${document.querySelector("#newtask input").value}</td>
+            </tr>
+          
+          </table>
+               
+                <button class="delete">
+                    <i class="far fa-trash-alt">x</i>
+                </button>
+            </div>
+        `;
 
-const todoInput = document.getElementById("new-task");
-const addButton = document.getElementsByTagName("button")[0];
-console.log('input',todoInput);
-console.log('input',addButton);
+    var current_tasks = document.querySelectorAll(".delete");
+    for (var i = 0; i < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        this.parentNode.remove();
+      };
+    }
+  }
+};
